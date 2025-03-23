af453f6e99989af5e96c5b1de3d2666e56293e75
guilherme2machado2 <bnwcrypee@gmail.com>
2025-03-23 20:59:04 -0300

    add dependecies on repo

---
+++ src/main/java/com/mobius/MobiusApplication.java
@@ -0,0 +1,13 @@
+package com.mobius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobius/MobiusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobiusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
